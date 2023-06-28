1f9e5c7c842d188569a80d9fdc5c9a78612ef913
Jaedson Silva <jaedson.dev@proton.me>
2023-06-27 22:34:14 -0300

    feat: create timer controller inputs

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,34 @@ function App() {
                     id='controlSwitch'
                 />
             </div>
+            <div className='timerArea'>
+                <div className='timerControl'>
+                    <label htmlFor="timerControlSwitch">Temporizador</label>
+                    <Switch
+                        onChange={checked => setChecked(checked)}
+                        checked={checked}
+                        checkedIcon={false}
+                        uncheckedIcon={false}
+                        id='timerControlSwitch'
+                    />
+                </div>
+                <div className="timerSelectorArea">
+                    <div className='timerSelectorForm'>
+                        <label htmlFor="minTimer">Liga às</label>
+                        <div id="minTimer">
+                            <input type="number" min={0} max={23} id="hourSelector" placeholder='Hora'/>
+                            <input type="number" min={0} max={59} id="minuteSelector" placeholder='Minutos'/>
+                        </div>
+                    </div>
+                    <div className='timerSelectorForm'>
+                        <label htmlFor="maxTimer">Desliga às</label>
+                        <div id='maxTimer'>
+                            <input type="number" min={0} max={23} id="hourSelector" placeholder='Hora'/>
+                            <input type="number" min={0} max={59} id="minuteSelector" placeholder='Minutos'/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
