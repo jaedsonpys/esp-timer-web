b851639aa249a05ea9d5274de5e57f38e6f081ea
Jaedson Silva <jaedson.dev@proton.me>
2023-06-27 22:31:06 -0300

    feat: rename App file extension

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Switch from 'react-switch';
+import { useState } from 'react';
+
+function App() {
+    const [checked, setChecked] = useState(false);
+    
+    return (
+        <>
+            
+        </>
+    );
+}
+
+export default App;
